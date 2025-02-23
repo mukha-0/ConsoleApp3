1212e3414f7f8dbb0648f68784de3808ab4fe451
mukha_0 <muhammadali8261@mail.ru>
2025-02-23 19:56:50 +0500

    changed

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,12 +1,15 @@
-﻿void minutsekundqilish(int n)
+﻿int yigindi(int x)
 {
-    int hours = n / 3600;
-    int minutes = (n % 3600) / 60;
-
-    Console.WriteLine($"{hours} soat {minutes} minut");
+    if (100 > x || x > 999)
+    {
+        int yuz = x / 100;
+        int on = x % 100 / 10;
+        int bir = x % 100 % 10 / 1;
+        return yuz + on + bir;
+    }
 }
 
 Console.WriteLine("Son: ");
-string? input = Console.ReadLine();
-var seconds = int.Parse(input);
-minutsekundqilish(seconds);
+string input = Console.ReadLine();
+var natija = int.Parse(input);
+Console.WriteLine(yigindi(natija));
