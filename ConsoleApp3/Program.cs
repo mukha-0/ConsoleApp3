1c7f964a4ebf9f8268f191f78ea2dcaf9dd98b9a
mukha_0 <muhammadali8261@mail.ru>
2025-02-24 15:04:00 +0500

    changed

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,31 +10,11 @@ class Program
         jaggedArray[1] = new int[] { 4, 5 };
         jaggedArray[2] = new int[] { 6, 7, 8, 9 };
 
-        int target = 5;
-
-        bool found = false;
+        Console.WriteLine("Har bir ichki qator uzunligi:");
 
         for (int i = 0; i < jaggedArray.Length; i++)
         {
-            for (int j = 0; j < jaggedArray[i].Length; j++)
-            {
-                if (jaggedArray[i][j] == target)
-                {
-                    found = true;
-                    break;
-                }
-            }
-            if (found)
-                break;
-        }
-
-        if (found)
-        {
-            Console.WriteLine(target + " jagged array ichida mavjud  .");
-        }
-        else
-        {
-            Console.WriteLine(target + " jagged array ichida mavjud emas  .");
+            Console.WriteLine("Massiv " + (i + 1) + " uzunligi: " + jaggedArray[i].Length);
         }
     }
 }
