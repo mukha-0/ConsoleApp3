7a340e52e61683ba5b1f4d8a1a11e9ee38b3ca50
mukha_0 <muhammadali8261@mail.ru>
2025-02-24 15:00:05 +0500

    changed

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,18 +10,31 @@ class Program
         jaggedArray[1] = new int[] { 4, 5 };
         jaggedArray[2] = new int[] { 6, 7, 8, 9 };
 
-        Console.WriteLine("Har bir ichki massiv teskari tartibda:");
+        int target = 5;
+
+        bool found = false;
 
         for (int i = 0; i < jaggedArray.Length; i++)
         {
-            Array.Reverse(jaggedArray[i]);
-
             for (int j = 0; j < jaggedArray[i].Length; j++)
             {
-                Console.Write(jaggedArray[i][j] + " ");
+                if (jaggedArray[i][j] == target)
+                {
+                    found = true;
+                    break;
+                }
             }
+            if (found)
+                break;
+        }
 
-            Console.WriteLine();
+        if (found)
+        {
+            Console.WriteLine(target + " jagged array ichida mavjud.");
+        }
+        else
+        {
+            Console.WriteLine(target + " jagged array ichida mavjud emas.");
         }
     }
 }
