c61dc3a287db040e946e235f1b37fe788d895d41
mukha_0 <muhammadali8261@mail.ru>
2025-02-24 15:18:07 +0500

    changed

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,17 +4,6 @@ class Program
 {
     static void Main()
     {
-        int[][] jaggedArray = new int[3][];
-
-        jaggedArray[0] = new int[] { 1, 2, 3 };
-        jaggedArray[1] = new int[] { 4, 5 };
-        jaggedArray[2] = new int[] { 6, 7, 8, 9 };
-
-        Console.WriteLine("Har bir ichki qator uzunligi:");
-
-        for (int i = 0; i < jaggedArray.Length; i++)
-        {
-            Console.WriteLine("Massiv " + (i + 1) + " uzunligi: " + jaggedArray[i].Length);
-        }
+      
     }
 }
