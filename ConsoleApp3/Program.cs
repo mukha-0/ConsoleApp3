f11118d0f20553dca30e92ea66df05d556811d24
mukha_0 <muhammadali8261@mail.ru>
2025-02-24 14:59:08 +0500

    changed

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,26 +1,26 @@
-﻿using ConsoleApp3;
+﻿using System;
 
-public class Program
+class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-
-
-
         int[][] jaggedArray = new int[3][];
-
 
         jaggedArray[0] = new int[] { 1, 2, 3 };
         jaggedArray[1] = new int[] { 4, 5 };
         jaggedArray[2] = new int[] { 6, 7, 8, 9 };
 
+        Console.WriteLine("Har bir ichki massiv teskari tartibda:");
 
         for (int i = 0; i < jaggedArray.Length; i++)
         {
+            Array.Reverse(jaggedArray[i]);
+
             for (int j = 0; j < jaggedArray[i].Length; j++)
             {
                 Console.Write(jaggedArray[i][j] + " ");
             }
+
             Console.WriteLine();
         }
     }
